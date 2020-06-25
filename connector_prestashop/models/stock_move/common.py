ab939ccdecc0ed8028d66f8974efb60dfb6dd861
Jesús Ventosinos Mayor <jesus@comunitea.com>
2020-06-25 15:38:11 +0200

    Update common.py

--- connector_prestashop/models/stock_move/common.py
+++ connector_prestashop/models/stock_move/common.py
@@ -39,7 +39,7 @@ class StockQuant(models.Model):
         for quant in self:
             location = quant.location_id
             super(StockQuant, self).write(vals)
-            if location in ps_locations:
+            if location in ps_locations or 'location_id' in vals and quant.location_id in ps_locations:
                 quant.invalidate_cache()
                 quant.product_id.update_prestashop_qty()
         return True
